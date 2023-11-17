3fadd7ccf9da7e913107124c0bc9ab48e253e110
luckyzhz <zhzwork@outlook.com>
2023-11-17 23:30:29 +0900

    labAlbum
    A few photo albums make up a gallery.

---
+++ program/js/components/labAlbum.js
@@ -0,0 +1,49 @@
+const labAlbum = {
+    data: function () {
+        return {
+            open: false,
+            currentImage: 0,
+        }
+    },
+    props: {
+        "album": Object,
+    },
+    methods: {
+        switchAlbum: function (event) {
+            if (!this.open) {
+                this.open = !this.open;
+            } else {
+                if (event.target === event.currentTarget) {
+                    this.open = !this.open;
+                }
+            }
+        },
+        toLastImage: function () {
+            this.currentImage--;
+        },
+        toNextImage: function () {
+            this.currentImage++;
+        },
+        toClose: function () {
+            this.open = false;
+        },
+    },
+    template:
+        `
+        <div class="lab-album" @click="switchAlbum">
+            <div class="caption" v-show="open">
+                {{album.images[currentImage].caption}}
+                <div class="close-button" @click.stop="toClose">X</div>
+            </div>
+            <div class="image-box">
+                <img :src="album.images[currentImage].link">
+                <div class="left-button" @click.stop="toLastImage" v-show="open && currentImage > 0">&lt;</div>
+                <div class="right-button" @click.stop="toNextImage" v-show="open && currentImage < album.images.length - 1">
+                    &gt;</div>
+                <div class="title" v-show="!open">{{album.title}}</div>
+            </div>
+        </div>
+        `
+}
+
+export { labAlbum };

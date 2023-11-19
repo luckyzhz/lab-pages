8e7969e70deaf0e2e5f2de163de8af0caf53d867
luckyzhz <zhzwork@outlook.com>
2023-11-20 01:50:59 +0900

    gallery.css

--- program/js/components/labAlbum.js
+++ program/js/components/labAlbum.js
@@ -10,12 +10,11 @@ const labAlbum = {
     },
     methods: {
         switchAlbum: function (event) {
-            if (!this.open) {
-                this.open = !this.open;
+            this.open = !this.open;
+            if (this.open) {
+                event.currentTarget.classList.add("open");
             } else {
-                if (event.target === event.currentTarget) {
-                    this.open = !this.open;
-                }
+                event.currentTarget.classList.remove("open");
             }
         },
         toPreviousImage: function () {
@@ -24,24 +23,20 @@ const labAlbum = {
         toNextImage: function () {
             this.currentImage++;
         },
-        toClose: function () {
-            this.open = false;
-        },
     },
     template:
         `
         <div class="lab-album" @click="switchAlbum">
             <div class="caption" v-show="open">
                 {{album.images[currentImage].caption}}
-                <div class="close-button" @click.stop="toClose">X</div>
             </div>
             <div class="image-box">
                 <img :src="album.images[currentImage].link">
                 <div class="left-button" @click.stop="toPreviousImage" v-show="open && currentImage > 0">&lt;</div>
                 <div class="right-button" @click.stop="toNextImage" v-show="open && currentImage < album.images.length - 1">
                     &gt;</div>
-                <div class="title" v-show="!open">{{album.title}}</div>
             </div>
+            <div class="title" v-show="!open">{{album.title}}</div>
         </div>
         `
 }

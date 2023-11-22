dc59b52e5aab8f62637e20dffb90428092ff0a53
luckyzhz <zhzwork@outlook.com>
2023-11-22 12:41:08 +0900

    responsive event listener

--- program/js/main.js
+++ program/js/main.js
@@ -1,5 +1,7 @@
 import { router } from "./router.js";
 
+import { carousel } from "./components/carousel.js";
+
 
 
 // router can access app's properties through ` router.app`
@@ -8,6 +10,7 @@ const app = new Vue({
     components: {
         // Components has been registered in routers
         // "lab-members": labMembers,
+        "carousel": carousel
     },
     data: {
     },
@@ -17,3 +20,25 @@ const app = new Vue({
     },
     router: router,
 });
+
+
+// 响应式
+// 获取和导航栏是否打开绑定的复选框
+const toggleNav = document.querySelector("#toggle-nav");
+// 从CSS获取断点变量的值
+const breakpointTablet = getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-tablet').trim();
+// 创建媒体查询
+const mq = window.matchMedia(`(max-width: ${breakpointTablet})`);
+// 定义处理媒体查询变化的函数
+function handleBreakpointChange(e) {
+    if (e.matches) {
+        toggleNav.checked = false;
+    } else {
+        toggleNav.checked = true;
+    }
+}
+
+// 监听媒体查询的变化
+mq.addEventListener('change', handleBreakpointChange);
+// 初始检查
+handleBreakpointChange(mq);
